e1852eecc101ecce0c27655f9d52c82f5c47b097
wudaming <wuariesming@gmial.com>
2022-08-15 22:00:39 +0800

    java notes

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  title: '笔记',
+  description: 'Just playing around',
+  themeConfig: {
+    sidebar: [
+      {
+        title: 'Java',   // 必要的
+        path: '/Java/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        collapsable: false, // 可选的, 默认值是 true,
+        sidebarDepth: 2,    // 可选的, 默认值是 1
+        children: [
+          '/Java/reference_type.md',
+          '/Java/lock.md'
+        ]
+      },
+    ]
+  }
+}
