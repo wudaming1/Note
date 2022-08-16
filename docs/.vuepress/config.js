0614833a761d44643f879154f4361da7c9410d52
wudaming <wuariesming@gmial.com>
2022-08-16 22:00:01 +0800

    android

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,6 +13,16 @@ module.exports = {
           '/Java/lock.md'
         ]
       },
+      {
+        title: 'Android',   // 必要的
+        path: '/Android/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        collapsable: false, // 可选的, 默认值是 true,
+        sidebarDepth: 2,    // 可选的, 默认值是 1
+        children: [
+          '/Android/decompile.md',
+          '/Android/directory.md'
+        ]
+      },
     ]
   }
 }
