98f33cb2db2417f6d308856f52967fdd8cb374ff
wudaming <wuariesming@gmial.com>
2022-08-16 22:37:33 +0800

    Java

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,11 +6,27 @@ module.exports = {
       {
         title: 'Java',   // 必要的
         path: '/Java/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-        collapsable: false, // 可选的, 默认值是 true,
+        collapsable: true, // 可选的, 默认值是 true,
         sidebarDepth: 2,    // 可选的, 默认值是 1
         children: [
           '/Java/reference_type.md',
-          '/Java/lock.md'
+          '/Java/lock.md',
+          '/Java/gc.md',
+          '/Java/history.md',
+          '/Java/keytool.md',
+          '/Java/innerClass.md',
+          '/Java/VM/bytecode.md',
+          '/Java/VM/ClassInit.md',
+          '/Java/VM/ClassLoader.md',
+          {
+            title: 'JDK5',
+            children: [
+              '/Java/JDK5/compileStructrue.md',
+              '/Java/JDK5/enum.md',
+              '/Java/JDK5/generic.md',
+              '/Java/JDK5/processor.md',
+            ]
+          },
         ]
       },
       {
